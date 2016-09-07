85cbe7648b89023367dab353f0b673162c522f94
AlexandraPV <your_email@your-domain.com>
2016-09-07 21:37:14 +0300

    test commit

---
+++ test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Alexandra on 07.09.16.
+//  Copyright © 2016 Alexandra. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello, World!\n");
+    return 0;
+}
