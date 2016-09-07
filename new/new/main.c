004626b861bcf359cf0e262b9fc912f39530701d
AlexandraPV <your_email@your-domain.com>
2016-09-07 21:43:05 +0300

    first commit

---
+++ new/new/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  new
+//
+//  Created by Alexandra on 07.09.16.
+//  Copyright © 2016 Alexandra. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("New!\n");
+    return 0;
+}
